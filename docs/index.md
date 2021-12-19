eef2201b126ee53a7100e465f0104dc26066fdb5
ruanqizhen <ruanqizhen@gmail.com>
2021-12-19 14:43:23 -0500

    test

---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+id: greeting
+title: 您好
+---
+
+## 来自 Docusaurus 的问候
+
+您准备好为您的开源项目创建文档网站了吗？
---
+++ docusaurus.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'LabVIEW 编程经验',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://ruanqizhen.github.io/test/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  projectName: 'labview_book', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/ruanqizhen/labview_book/edit/main/',
+		  routeBasePath: '/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'LabVIEW 编程经验',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+		  href: '/'
+        },
+        items: [
+          {
+            href: 'https://github.com/ruanqizhen/labview_book',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+	
+  i18n: {
+    defaultLocale: "zh-CN",
+    locales: ["zh-CN"],
+    // locales: ["zh-CN", "en"],
+    localeConfigs: {
+      "zh-CN": {
+        label: "中文",
+      },
+      en: {
+        label: "English",
+      },
+    },
+  },
+};
+
+module.exports = config;
