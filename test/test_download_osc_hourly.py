43ac45c5ca37d74e2b448ab1f1fc9aceb9a088dd
ruanqizhen <ruanqizhen@gmail.com>
2024-05-17 12:10:48 -0400

    Synchronizing job test_download_osc_hourly from AWS Glue at 2024/05/17 16:10:47

---
+++ test/test_download_osc_hourly.py
@@ -0,0 +1,205 @@
+import sys
+from awsglue.transforms import *
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+from posixpath import join as urljoin
+from gzip import GzipFile
+from xml.etree import ElementTree
+from datetime import datetime, timezone
+import requests
+
+from osmdiff.osm.osm import *
+
+REPLICATION_URL = "https://planet.openstreetmap.org/replication"
+
+
+class OSMChange(object):
+    def __init__(
+        self,
+        url=REPLICATION_URL,
+        frequency="minute",
+        file=None,
+        sequence_number=None,
+        debug=False,
+    ):
+        self.base_url = url
+        self.debug = debug
+        self.create = []
+        self.modify = []
+        self.delete = []
+        if file:
+            with open(file, "r", encoding="utf8") as fh:
+                self._parse_stream(fh)
+        else:
+            self._frequency = frequency
+            self._sequence_number = sequence_number
+
+    def get_state(self):
+        """Get the current state from the OSM API"""
+        state_url = urljoin(self.base_url, self._frequency, "state.txt")
+        response = requests.get(state_url, timeout=5)
+        if response.status_code != 200:
+            return False
+        for line in response.text.split("\n"):
+            if self.debug:
+                print(line)
+            if line.startswith("sequenceNumber"):
+                self._sequence_number = int(line[15:])
+            if line.startswith("timestamp"):
+                time_str = line[10:].replace('Z', '').replace("\\", '')
+                date_obj = datetime.fromisoformat(time_str).replace(tzinfo=timezone.utc)
+                self.date = date_obj.strftime("%Y-%m-%d")
+
+        return True
+
+    def _build_sequence_url(self):
+        seqno = str(self._sequence_number).zfill(9)
+        url = urljoin(
+            self.base_url,
+            self._frequency,
+            seqno[:3],
+            seqno[3:6],
+            "{}{}".format(seqno[6:], ".osc.gz"),
+        )
+        if self.debug:
+            print(url)
+        return url
+
+    def _parse_stream(self, stream):
+        for event, elem in ElementTree.iterparse(stream):
+            # if self.debug:
+            #     print(event, elem)
+            if elem.tag in ("create", "modify", "delete"):
+                self._build_action(elem)
+                if self.debug:
+                    print("======={action}========".format(action=elem.tag))
+
+    def _build_action(self, elem):
+        for thing in elem:
+            o = OSMObject.from_xml(thing)
+            self.__getattribute__(elem.tag).append(o)
+            if self.debug:
+                print(o)
+                print(o.attribs)
+                print(o.tags)
+                print(o.bounds)
+
+    def retrieve(self, clear_cache=False) -> int:
+        """
+        Retrieve the OSM diff corresponding to the OSMChange sequence_number.
+        """
+        if not self._sequence_number:
+            raise Exception("invalid sequence number")
+        if clear_cache:
+            self.create, self.modify, self.delete = ([], [], [])
+        try:
+            r = requests.get(self._build_sequence_url(), stream=True, timeout=30)
+            if r.status_code != 200:
+                return r.status_code
+            gzfile = GzipFile(fileobj=r.raw)
+            self._parse_stream(gzfile)
+            return r.status_code
+        except ConnectionError:
+            # FIXME catch this?
+            return 0
+
+    @classmethod
+    def from_xml(cls, path):
+        new_osmchange_obj = cls()
+        new_osmchange_obj._parse_stream(path)
+        return new_osmchange_obj
+
+    def sequence_number(self):
+        return self._sequence_number
+
+    def set_sequence_number(self, sn):
+        self._sequence_number = int(sn)
+
+    sequence_number = property(sequence_number, set_sequence_number)
+
+    def __repr__(self):
+        return "OSMChange ({create} created, {modify} modified, {delete} deleted)".format(
+            create=len(self.create), modify=len(self.modify), delete=len(self.delete)
+        )
+        
+
+
+node_schema = "id bigint, version bigint, timestamp string, uid bigint, user string, changeset bigint, tags map<string,string>, lat double, lon double"
+way_schema = "id bigint, version bigint, timestamp string, uid bigint, user string, changeset bigint, tags map<string,string>, nds array<bigint>"
+relation_schema = "id bigint, version bigint, timestamp string, uid bigint, user string, changeset bigint, tags map<string,string>, members array<map<string,string>>"
+
+
+# Download new osc files
+osc = OSMChange(frequency="hour")
+osc.get_state()
+osc.retrieve()
+
+ds = osc.date
+
+# Save new osc data into tables
+for change in ["create", "modify", "delete"]:
+    node_data = []
+    way_data = []
+    relation_data = []
+    items = getattr(osc, change)
+    node_total_number = len(items)
+    
+    for obj in items:
+        if type(obj) == Node:
+            node_data.append([
+                int(obj.attribs["id"]), 
+                int(obj.attribs["version"]), 
+                obj.attribs["timestamp"], 
+                int(obj.attribs["uid"]), 
+                obj.attribs["user"], 
+                int(obj.attribs["changeset"]), 
+                obj.tags,
+                float(obj.attribs["lat"]), 
+                float(obj.attribs["lon"]),
+            ])
+        elif type(obj) == Way:
+            way_data.append([
+                int(obj.attribs["id"]), 
+                int(obj.attribs["version"]), 
+                obj.attribs["timestamp"], 
+                int(obj.attribs["uid"]), 
+                obj.attribs["user"], 
+                int(obj.attribs["changeset"]), 
+                obj.tags,
+                [int(n.attribs["ref"]) for n in obj.nodes],
+            ])
+        elif type(obj) == Relation:    
+            relation_data.append([
+                int(obj.attribs["id"]), 
+                int(obj.attribs["version"]), 
+                obj.attribs["timestamp"], 
+                int(obj.attribs["uid"]), 
+                obj.attribs["user"], 
+                int(obj.attribs["changeset"]), 
+                obj.tags,
+                [m.attribs for m in obj.members],
+            ])
+        else:
+            pass
+
+    spark.createDataFrame(node_data, node_schema).write.mode('append').parquet(
+       f"s3://omf-airflow-athena-results/osm_geometry/diff_node_{change}/ds={ds}/"
+    )
+    
+    spark.createDataFrame(way_data, way_schema).write.mode('append').parquet(
+        f"s3://omf-airflow-athena-results/osm_geometry/diff_way_{change}/ds={ds}/"
+    )
+    
+    spark.createDataFrame(relation_data, relation_schema).write.mode('append').parquet(
+        f"s3://omf-airflow-athena-results/osm_geometry/diff_relation_{change}/ds={ds}/"
+    )
+
+    del node_data
+    del way_data
+    del relation_data
+
